93460458ca7aa2e33cd92b2a577da978f3125eb2
Alan Moy <alan.moy@colorado.edu>
2015-10-29 20:16:45 -0600

    Test db_interface.py

---
+++ server/db_interface_test.py
@@ -0,0 +1,41 @@
+import unittest
+import db_interface as db
+import random
+import string
+import math
+import os
+
+def rand_str(n):
+    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(n))
+
+def rand_db():
+    return rand_str(8) + '.db'
+
+def usage_from_timestamp(timestamp):
+    return int(math.sin(timestamp) * 100)
+
+class TestDBMethods(unittest.TestCase):
+    def test_add_query(self):
+        dbname = rand_db()
+        handle = db.connect(dbname)
+        try:
+            db.create_outletify_tables(handle)
+
+            for i in range(100):
+                db.add_row(i, usage_from_timestamp(i), handle)
+            q1 = db.query_by_timestamp(10, handle)
+            self.assertEqual(q1, [(10, usage_from_timestamp(10))])
+
+            q2 = db.query_by_timestamp((5, 15), handle)
+            expected_usages = []
+            for i in range(5, 16):
+                expected_usages.append((i, usage_from_timestamp(i)))
+            for pair in zip(q2, expected_usages):
+                self.assertEqual(pair[0], pair[1])
+        finally:
+            db.close_handle(handle)
+            os.remove(dbname)
+
+if __name__ == '__main__':
+    unittest.main()
+
