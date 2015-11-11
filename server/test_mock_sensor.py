b142972c9fec4072069e83e7e4aed85480667cc3
Alan Moy <alan.moy@colorado.edu>
2015-11-10 21:41:21 -0700

    Add script to run all server tests

--- server/test_mock_sensor.py
+++ server/test_mock_sensor.py
@@ -7,6 +7,12 @@ from time import sleep
 class TestMockSensor(unittest.TestCase):
     ''' Test the sensor interface to the server. Requires that the server be
     running. '''
+    def setUp(self):
+        cmdline = ['./uwsgi', '--http', ':9090', '--wsgi-file', 'main.py']
+        self.proc = subprocess.Popen(cmdline, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        sleep(2)
+    def tearDown(self):
+        self.proc.kill()
     def test_post_insert(self):
         q1 = db.query_by_timestamp(0)
         mock_sensor.post('{"function": "insert", "usage": 1, "timestamp": 0}')
@@ -25,11 +31,5 @@ class TestMockSensor(unittest.TestCase):
         self.assertEqual(q1, q2)
 
 if __name__ == '__main__':
-    cmdline = ['./uwsgi', '--http', ':9090', '--wsgi-file', 'main.py']
-    try:
-        proc = subprocess.Popen(cmdline, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-        sleep(2)
-        unittest.main()
-    finally:
-        proc.kill()
+    unittest.main()
 
