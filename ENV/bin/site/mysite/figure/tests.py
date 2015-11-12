bc5a1f4384bd2e0173c44ca8d955e8ced26d32ad
Jon Wehrend <jon@engr2-16-108-dhcp.int.colorado.edu>
2015-11-11 21:12:36 -0700

    Updated database unittest

--- ENV/bin/site/mysite/figure/tests.py
+++ ENV/bin/site/mysite/figure/tests.py
@@ -1,16 +1,10 @@
 from django.test import TestCase
 from figure.models import Usage
+   
+class UsageTestCase(TestCase):
+	def setUp(self):
+		self.entry = Usage.objects.create(device_usage=10, time_stamp='2015-11-11 20:20')
 
-# Create your tests here.
-class FigureTest(TestCase):
-
-	def test_str(self):
-
-		entry = Usage(device_usage=0, time_stamp='2015-10-30 15:06')
-
-		self.assertEquals(
-			str(entry),
-			,
-			)
-
-		
+	def test_entry(self):
+		self.assertEqual(self.entry.device_usage, 10)
+		self.assertEqual(self.entry.time_stamp, '2015-11-11 20:20')
