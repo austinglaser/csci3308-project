4d980709d93b2c54a7ff05280d8cd8dfb3940018
Jon Wehrend <jon@Jon-Wehrends-MacBook-Pro.local>
2015-11-20 19:11:42 -0700

    Added chartit scripts and required libraries, modified views.py, index.html and settings.py

--- ENV/bin/site/mysite/figure/views.py
+++ ENV/bin/site/mysite/figure/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.template import RequestContext, loader
 from django.http import HttpResponse
 from .models import Usage
 from django.http import Http404
+from chartit import DataPool, Chart
 
 def index(request):
 	usage_list = Usage.objects
@@ -12,6 +13,36 @@ def index(request):
 
 		})
 	return HttpResponse(template.render(context))
+
+def chart_view(request):
+	usagedata = \
+		DataPool(
+			series=
+			[{'options': {
+				'source': Usage.objects.all()},
+				'terms': [
+					'device_usage',
+					'time_stamp']}
+			])
+
+	cht = Chart(
+			datasource = usagedata,
+			series_options = 
+				[{'options': {
+					'type': 'line',
+					'stacking': False },
+					'terms': {
+						'time_stamp' : [
+						'device_usage' ]
+					}}],
+			chart_options = 
+				{'title': {
+					'text': 'Usage Data' },
+				'xAxis': {
+					'title': {
+						'text': 'Date'}}})
+
+	return render_to_response({'usagechart': cht})
 
 def detail(request, usage_id):
 	try:
